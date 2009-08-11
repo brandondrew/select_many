50db2c71248414046ed0be6a58df3ef0356c498e
Corey Burrows <corey.burrows@centro.net>
2009-08-11 11:29:01 -0500

    Add selected options to an unordered list.

--- test/test_select_many.js
+++ test/test_select_many.js
@@ -1,11 +1,25 @@
 jQuery(function() {
-  module("selectMany");
-
-  test("1 === 1", function() {
-    ok(1 === 1);
+  module("selectMany", {
+    setup: function() {
+      jQuery('select.select-many').selectMany();
+      this.harleys = jQuery('#harleys');
+      this.selectedList = this.harleys.prev('ul.select-many-selected');
+    }
   });
 
-  test("1 === 2", function() {
-    ok(1 === 2);
+  test("should create a ul before the select element with the class select-many-selected", function() {
+    ok(this.selectedList.get(0));
+  });
+
+  test("selecting an option should create a corresponding list item", function() {
+    this.harleys.find('option[value=3]').click();
+    equals(this.selectedList.find('li').size(), 1);
+    equals(this.selectedList.find('li').text(), 'Softail Deluxe');
+  });
+
+  test("selecting an option should set it to disabled", function() {
+    var item = this.harleys.find('option[value=1]');
+    item.click();
+    equals(item.attr('disabled'), true);
   });
 });
