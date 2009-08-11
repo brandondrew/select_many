5ed8abb06ab8318b7107c00cf5700ddd7a1276a9
Corey Burrows <corey.burrows@centro.net>
2009-08-11 15:34:34 -0500

    Handle multiple selects on one page and preselected values.

--- test/test_select_many.js
+++ test/test_select_many.js
@@ -16,18 +16,20 @@ jQuery(function() {
     setup: function() {
       jQuery('select.select-many').selectMany();
       this.harleys = jQuery('#harleys');
-      this.selectedList = this.harleys.prev('ul.select-many-selected');
+      this.hondas = jQuery('#hondas');
+      this.harleysSelected = this.harleys.prev('ul.select-many-selected');
+      this.hondasSelected = this.hondas.prev('ul.select-many-selected');
     }
   });
 
   test("should create a ul before the select element with the class select-many-selected", function() {
-    ok(this.selectedList.get(0));
+    ok(this.harleysSelected.get(0));
   });
 
   test("selecting an option should create a corresponding list item", function() {
     changeSelect(this.harleys, '3');
-    equals(this.selectedList.find('li').size(), 1);
-    equals(this.selectedList.find('li').text(), 'Softail Deluxe');
+    equals(this.harleysSelected.find('li').size(), 1);
+    equals(this.harleysSelected.find('li').text(), 'Softail Deluxe');
   });
 
   test("selecting an option should set it to disabled", function() {
@@ -37,22 +39,22 @@ jQuery(function() {
   });
 
   test("clicking the select element does not add items to the selected list", function() {
-    equals(this.selectedList.find('li').size(), 0);
+    equals(this.harleysSelected.find('li').size(), 0);
     this.harleys.click();
-    equals(this.selectedList.find('li').size(), 0);
+    equals(this.harleysSelected.find('li').size(), 0);
   });
 
   test("clicking a disabled option does nothing", function() {
     var item = this.harleys.find('option[value=1]');
     changeSelect(this.harleys, '1');
-    equals(this.selectedList.find('li').size(), 1);
+    equals(this.harleysSelected.find('li').size(), 1);
     item.click();
-    equals(this.selectedList.find('li').size(), 1);
+    equals(this.harleysSelected.find('li').size(), 1);
   });
 
   test("selecting an option creates a hidden input field inside its selected list item", function() {
     changeSelect(this.harleys, '3');
-    var li = this.selectedList.find('li');
+    var li = this.harleysSelected.find('li');
     var input = li.find('input[type=hidden]');
 
     ok(input.get(0));
@@ -60,14 +62,14 @@ jQuery(function() {
  
   test("a selected option's hidden input field should have its value set to the options value", function() {
     changeSelect(this.harleys, '4');
-    var input = this.selectedList.find('li input[type=hidden]');
+    var input = this.harleysSelected.find('li input[type=hidden]');
 
     equals(input.attr('value'), '4');
   });
  
   test("a selected option's hidden input field should have its name set to the parent select's name", function() {
     changeSelect(this.harleys, '4');
-    var input = this.selectedList.find('li input[type=hidden]');
+    var input = this.harleysSelected.find('li input[type=hidden]');
 
     equals(input.attr('name'), 'harleys[]');
   });
@@ -78,18 +80,23 @@ jQuery(function() {
 
   test("clicking a selected list item removes it from the list", function() {
     changeSelect(this.harleys, '4');
-    var li = this.selectedList.find('li');
+    var li = this.harleysSelected.find('li');
 
-    equals(this.selectedList.find('li').size(), 1);
+    equals(this.harleysSelected.find('li').size(), 1);
     li.click();
-    equals(this.selectedList.find('li').size(), 0);
+    equals(this.harleysSelected.find('li').size(), 0);
   });
 
   test("clicking a selected list item enables its option in the select", function() {
     var option = this.harleys.find('option[value=4]');
     changeSelect(this.harleys, '4');
     equals(option.attr('disabled'), true);
-    this.selectedList.find('li').click();
+    this.harleysSelected.find('li').click();
     equals(option.attr('disabled'), false);
+  });
+
+  test("preselected options should be automatically added to selected list", function() {
+    equals(this.hondasSelected.find('li').size(), 1);
+    equals(this.hondasSelected.find('li').text(), 'Hornet');
   });
 });
